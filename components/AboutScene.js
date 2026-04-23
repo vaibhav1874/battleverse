7fb3c243fcaff0164f46b6af26a9d8bc80a8081e
Vaibhav Gurjar <vaibhavgurjar900@gmail.com>
2026-04-23 21:10:05 +0530

    Replace generic 3D blob with stylized floating microphone on About page

--- components/AboutScene.js
+++ components/AboutScene.js
@@ -30,36 +30,64 @@ export default function AboutScene() {
 
   return (
     <>
-      <Float speed={4} rotationIntensity={1} floatIntensity={2}>
-        <TorusKnot ref={mainRef} args={[1, 0.4, 256, 64]} position={[0, 0, 0]}>
-          <MeshDistortMaterial
-            color="#00a67a"
-            speed={4}
-            distort={0.4}
-            roughness={0.1}
-            metalness={0.9}
-            emissive="#00a67a"
-            emissiveIntensity={0.2}
-          />
-        </TorusKnot>
+      <Float speed={2} rotationIntensity={1.5} floatIntensity={2}>
+        <group ref={mainRef}>
+          {/* Mic Grill */}
+          <Sphere args={[0.8, 64, 64]} position={[0, 1, 0]}>
+            <meshStandardMaterial
+              color="#2a2a2a"
+              roughness={0.2}
+              metalness={1}
+              bumpScale={0.02}
+              emissive="#00a67a"
+              emissiveIntensity={0.2}
+            />
+          </Sphere>
+          
+          {/* Mic Grill Line */}
+          <mesh position={[0, 1, 0]}>
+            <torusGeometry args={[0.81, 0.02, 16, 100]} />
+            <meshBasicMaterial color="#00a67a" />
+          </mesh>
+
+          {/* Mic Neck */}
+          <mesh position={[0, 0.2, 0]}>
+            <cylinderGeometry args={[0.5, 0.45, 0.4, 32]} />
+            <meshStandardMaterial color="#111" metalness={0.8} roughness={0.2} />
+          </mesh>
+
+          {/* Mic Handle */}
+          <mesh position={[0, -1, 0]}>
+            <cylinderGeometry args={[0.45, 0.35, 2, 32]} />
+            <meshStandardMaterial color="#050505" metalness={0.5} roughness={0.5} />
+          </mesh>
+
+          {/* Glowing Accents on handle */}
+          <mesh position={[0, -0.8, 0]}>
+            <cylinderGeometry args={[0.46, 0.46, 0.05, 32]} />
+            <meshBasicMaterial color="#00a67a" />
+          </mesh>
+        </group>
       </Float>
 
-      <Float speed={2} rotationIntensity={2}>
+      {/* Orbiting Ring */}
+      <Float speed={3} rotationIntensity={2}>
         <mesh ref={ringRef}>
-          <torusGeometry args={[2.2, 0.02, 16, 100]} />
-          <meshBasicMaterial color="#00f3ff" />
+          <torusGeometry args={[2.5, 0.015, 16, 100]} />
+          <meshBasicMaterial color="#00f3ff" transparent opacity={0.3} />
         </mesh>
       </Float>
 
+      {/* Floating Particles/Notes */}
       <Float speed={5} floatIntensity={3}>
-        <Sphere args={[0.2, 32, 32]} position={[2, 2, -1]}>
-          <MeshWobbleMaterial color="#ff003c" speed={3} factor={0.6} />
+        <Sphere args={[0.08, 16, 16]} position={[2, 1, -1]}>
+          <meshBasicMaterial color="#00a67a" />
         </Sphere>
       </Float>
 
-      <Float speed={3} floatIntensity={1}>
-        <Sphere args={[0.15, 32, 32]} position={[-2, -1.5, 1]}>
-          <MeshWobbleMaterial color="#00f3ff" speed={2} factor={0.4} />
+      <Float speed={4} floatIntensity={2}>
+        <Sphere args={[0.05, 16, 16]} position={[-1.5, -2, 1]}>
+          <meshBasicMaterial color="#00f3ff" />
         </Sphere>
       </Float>
     </>
